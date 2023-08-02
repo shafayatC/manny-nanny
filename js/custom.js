63e2b2417bc0f31f05107d183d3756ea83610fb5
shafayatC <shafayatplay@gmail.com>
2023-08-02 16:59:02 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+const menuBtn = document.getElementById("mobileMenu"); 
+const menu = document.getElementById("menuWrap"); 
+
+const stick_1 = document.getElementById("stick_1");
+const stick_2 = document.getElementById("stick_2");
+// const stick_3 = document.getElementById("stick_3");
+
+var triggerBool = true
+
+menuBtn.addEventListener("click", ()=>{
+
+    if(triggerBool){
+        menu.style.visibility = "visible";
+        menu.style.opacity = "1";
+        menu.style.transform = "scaleY(1)"
+
+        /* menu button animation */
+        stick_2.style.display = 'block'; 
+        stick_1.style.display = 'none';
+
+        
+    }else {
+        menu.style.visibility = "hidden";
+        menu.style.opacity = "0";
+        menu.style.transform = "scaleY(0)"
+        
+        /* menu button animation */
+        stick_2.style.display = 'none'; 
+        stick_1.style.display = 'block';
+    }
+     triggerBool = !triggerBool;
+})
+
+
+/* header sticky start */
+const header = document.getElementById("headerWrap");
+// Get the header element
+
+// Get the initial position and height of the header
+const headerOffsetTop = header.offsetTop;
+const headerHeight = header.offsetHeight;
+
+// Function to handle the scrolling event
+function handleScroll() {
+  // Get the current scroll position and viewport height
+  const scrollPosition = window.pageYOffset;
+
+  // Calculate the ending position of the header
+  const endingPosition = headerOffsetTop + headerHeight;
+
+  // Check if the scroll position is within the range of the header's ending position
+  if (scrollPosition >= endingPosition) {
+    // If yes, add a class to the header to make it sticky
+    header.classList.add("sticky");
+  } else {
+    // If not, remove the class to make the header back to its original position
+    header.classList.remove("sticky");
+  }
+}
+
+// Attach the handleScroll function to the scroll event
+window.addEventListener("scroll", handleScroll);
+
+
+/* header sticky end */
