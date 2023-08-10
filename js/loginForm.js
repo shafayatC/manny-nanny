ca8a28821a969a889a916b3f67adfed866e58152
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-08-10 11:41:12 +0600

    fixed some issue on login and registration page

--- js/loginForm.js
+++ js/loginForm.js
@@ -1,33 +1,31 @@
 function showLabel(fieldId) {
     const label = document.querySelector(`label[for="${fieldId}"]`);
     const label2 = document.querySelector(`label[for="password"]`);
-    const bgUrl = document.querySelector(".span-visible")
+    const iconUrl = document.querySelector(".span-visible")
     const passwordInputVisible = document.getElementById('password');
 
     if (fieldId === "password") {
-
         label2.style.color = "#C81E69";
         if (passwordInputVisible.type === "password") {
-            bgUrl.src = "../assets/icons/icons-final-colors-24x24/icon_visibility-on-pink.svg";
+            iconUrl.src = "../assets/icons/icons-final-colors-24x24/icon_visibility-on-pink.svg";
         }
-        label.style.transform = "translateY(-40px)";
+
     }
+    label.style.transform = "translateY(-38px)";
 }
 
 function hideLabel(fieldId) {
     const input = document.getElementById(fieldId);
-    const bgUrl = document.querySelector(".span-visible")
+    const iconUrl = document.querySelector(".span-visible")
     if (!input.value) {
         const label = document.querySelector(`label[for="${fieldId}"]`);
         label.style.transform = "translateY(-12px)";
         label.style.color = "#707070";
         const btn = document.getElementById("login-btn");
         btn.style.backgroundColor = "#EFC5D8"
-        bgUrl.src = "../assets/icons/icons-black-24x24/icon_visibility-on.svg";
-        // bgUrl.style.image = "url('../assets/icons/icons-black-24x24/icon_visibility-on.svg')";
+        iconUrl.src = "../assets/icons/icons-black-24x24/icon_visibility-on.svg";
     }
 }
-
 function onPasswordChange() {
     const passwordInput = document.getElementById("password");
     const btn = document.getElementById("login-btn");
@@ -38,7 +36,6 @@ function onPasswordChange() {
         btn.style.backgroundColor = "#EFC5D8";
     }
 }
-
 const passwordInput = document.getElementById("password");
 passwordInput.addEventListener("input", onPasswordChange);
 
